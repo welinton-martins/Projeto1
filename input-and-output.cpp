d3463a01da8341e46016d6dc48fd47aec5c51409
welinton-martins <welintonmartins4@outlook.com>
2021-08-08 00:58:03 +0000

    :smile:

--- input-and-output.cpp
+++ input-and-output.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 
 int main() {
-    char nome[] 50;
-
+    char nome[50];
     cout << "Por favor, entre com o seu nome: ";
     cin >> nome;
-    cout << "Seu nome e: " << nome << endl;
-
+    cout << "Seu nome: " << nome << endl;
     return 0;
 }
